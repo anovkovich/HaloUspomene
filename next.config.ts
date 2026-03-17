83f2bf384b7df1f669c975cf74a49f1627b51c9d
Aleksa <aleksa.novkovic@gmail.com>
2026-03-17 12:59:25 +0100

    no-cache: must-revalidate

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   trailingSlash: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
