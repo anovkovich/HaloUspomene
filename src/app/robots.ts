e2c17efe376d8cab081882969301d39127166387
Aleksa <aleksa.novkovic@gmail.com>
2026-03-25 20:43:21 +0100

    Remove /recenzija from robots.txt so Instagram crawler can scrape OG tags

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -25,7 +25,6 @@ export default function robots(): MetadataRoute.Robots {
           "/pozivnica/*/audio-knjiga",
           "/deciji-rodjendan/*/portal",
           "/deciji-rodjendan/*/prijava",
-          "/recenzija",
         ],
       },
       {
